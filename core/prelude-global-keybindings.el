3d7c1d84d9591b19b46a1cb38c06304727c39d89
Henry Robinson <henry@cloudera.com>
2013-06-17 16:09:02 -0700

    Small bugfixes

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -118,7 +118,7 @@
 (key-chord-define-global "jl" 'ace-jump-line-mode)
 (key-chord-define-global "jk" 'ace-jump-char-mode)
 (key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
-(key-chord-define-global "uu" 'undo-tree-visualize)
+; (key-chord-define-global "uu" 'undo-tree-visualize)
 
 (key-chord-mode +1)
 
